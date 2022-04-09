a891f9b262fe9e9425a994645064c5c5623f08da
jpcp0614 <jpcp0614@gmail.com>
2022-04-08 22:08:05 -0300

    Class Race - Ok -> Requisito 01

---
+++ src/Races/Race.ts
@@ -0,0 +1,15 @@
+export default abstract class Race {
+  constructor(
+    private readonly _name: string,
+    private readonly _dexterity: number,
+  ) { }
+
+  get name(): string { return this._name; }
+  get dexterity(): number { return this._dexterity; }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
