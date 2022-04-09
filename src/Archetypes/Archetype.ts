dcde3df4a01bde73dc6ea7899e7a03529cf3840e
jpcp0614 <jpcp0614@gmail.com>
2022-04-08 23:21:56 -0300

    Class Archetype - Ok -> Requisito 04

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,29 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  private readonly _special: number;
+  private readonly _cost: number;
+
+  constructor(private readonly _name: string) {
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
